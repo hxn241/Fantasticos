eb3ad9d2ca75087074003691bc10375fdb5401bb
Xavi Mañá <xavier.manya@gmail.com>
2020-11-19 20:19:33 +0100

    script_fase_1

--- test_hacker.sh
+++ test_hacker.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
-mkdir ~/ref_repo
-git clone https://github.com/rgomezh/campechano.git
+declare -A repos;
+
+repos[campechanos]='https://github.com/rgomezh/campechano.git'
+repos[goodshellers]='https://github.com/ruizber23/goodshellers.git'
+
+repo=~/ref_repo;
+mkdir "$repo";
+cd  "$repo";
+git clone "${repos[campechanos]}";
+
+declare -a dir;
+dir=$(find "$repo" -type f -name "*.out");
+echo "${dir[@]}";
+
 
